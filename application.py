870feaee010d08556df835acc0a7ee21ba0cf78f
Hrutikkhot <hrutikkhot18@gmail.com>
2023-12-29 19:07:02 +0530

    EB config

--- application.py
+++ application.py
@@ -40,4 +40,4 @@ def predict_species():
         return render_template('index.html', prediction=prediction, prob=prob)        
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', debug=True) 
+    app.run(host='0.0.0.0', debug=False) 
